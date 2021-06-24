2b42fa43abed6b93bf9bda24b9f09a7f482233bb
dvitorio <61206465+dvitorio@users.noreply.github.com>
2021-06-24 09:23:33 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function calcularIMC(){
-    var peso = parseFloat(document.getElementById("peso").value);
-    var altura = parseFloat(document.getElementById("altura").value);
+    var peso = document.getElementById("peso").value.replace(",", ".");
+    var altura = document.getElementById("altura").value.replace(",", ".");
         if((peso>0 && peso<=250 || altura>1.00 && altura<=2.72)){
-            var imc = peso/(altura*altura);
+            var imc = parseFloat(peso)/(parseFloat(altura)*parseFloat(altura));
             document.getElementById("imc-input").value = imc.toFixed(2);
         }
 }
